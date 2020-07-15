1211ae7513c77ec723dad62a8b28bcd303a20bd3
Raveen K Sharma <raveen08@gmail.com>
2020-07-15 18:30:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,7 +13,11 @@ def sort_array_char_count(array)
   end  
 end
 
-def swap_elements
+def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
+end
+
+def reverse_array(array)
+  array.reverse
 end  
